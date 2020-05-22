b23ea8e02a322d5a8cad3ce9938cc9fcc2348ca0
Takaf19 <tk.f19ms@gmail.com>
2020-05-23 01:15:40 +0900

    Update view

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
   <div id="app">
-    <router-view/>
+    <hedderitem @receiveComponentId="changeComponent" />
+    <router-view />
   </div>
 </template>
 
 <script>
+import Hedder from './components/hedder.vue'
+
 export default {
-  name: 'App'
-}
+  name: 'App',
+  data () {
+    return {
+      componentId: 0,
+    }
+  },
+  components: {
+    'hedderitem': Hedder,
+    // 'user-show': UserShow,
+  },
+  methods: {
+    changeComponent (id) {
+      debugger
+      this.componentId = id
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
---
+++ src/components/hedder.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id='hedder'>
+    <ul>
+      <li @click="changeId(0)"><a href="/">top</a></li>
+      <li @click="changeId(1)"><p>漢検３級</p></li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data () {
+    return {
+      componentId: 0,
+    }
+  },
+  watch: {
+    componentId: function () {
+      this.$emit('receiveComponentId', this.componentId);
+    },
+  },
+  methods: {
+    changeId(id) {
+      this.componentId = id;
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  display: flex;
+  justify-content: space-around;
+  list-style: none;
+}
+
+li {
+  
+}
+</style>
