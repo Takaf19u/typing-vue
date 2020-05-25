ccddc3d500ffb25958f9064982924beb2abd7558
Takaf19 <tk.f19ms@gmail.com>
2020-05-25 23:46:28 +0900

    add fortawesome

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,17 @@ import Router from 'vue-router'
 import Main from '@/components/typingMain'
 import Top from '@/components/top'
 
+/* アイコン用設定ここから */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas, far)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+/* ここまで */
+
 Vue.use(Router)
 
 export default new Router({
