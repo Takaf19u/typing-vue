d4439e7141d0668b4faf2995c9bacc6890f7460f
Takaf19 <tk.f19ms@gmail.com>
2020-05-26 22:08:11 +0900

    Update hedder

--- src/components/hedder.vue
+++ src/components/hedder.vue
@@ -1,8 +1,8 @@
 <template>
   <div id='hedder'>
     <ul>
-      <li @click="changeId(0)"><router-link to="/">top</router-link></li>
-      <li @click="changeId(1)"><router-link to="/typing">漢検３級</router-link></li>
+      <li @click="changeId(0)"><router-link to="/">TOP</router-link></li>
+      <li @click="changeId(1)"><router-link to="/typing">漢検三級</router-link></li>
     </ul>
   </div>
 </template>
@@ -34,16 +34,28 @@ export default {
   z-index: 10;
   height: 50px;
   width: 100%;
-  background: green;
+  background: rgb(1, 18, 63);
 }
+#hedder::after {
+  content: "";
+  position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: -10px;
+  z-index: -1;
+    border-bottom: 20px dashed rgb(1, 18, 63);
+}
+
 
 ul {
   list-style:  none;
   display: flex;
-  justify-content: space-around;
   line-height: 50px;
 }
 li {
+  font-family: "nicoMoji";
+  margin: 0 30px;
 }
 
 a {
