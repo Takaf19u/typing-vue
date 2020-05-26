4224dd357cc4e7222b1c4bae6f086416688d4286
Takaf19 <tk.f19ms@gmail.com>
2020-05-26 22:07:51 +0900

    add vueinview

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+require('intersection-observer');
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/sass/font.scss'
+import './css/ress.css'
+import Inview from 'vueinview'
 
+
+Vue.use(Inview);
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
